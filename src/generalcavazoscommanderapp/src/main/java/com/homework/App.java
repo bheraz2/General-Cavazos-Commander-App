b16d5e05d9d8b2ff5c2679c44c1c94478701ece5
Bryan Heraz <bherazbackup2@gmail.com>
2023-11-09 17:59:24 -0800

    Stack been declared and created method printMenu

--- src/generalcavazoscommanderapp/src/main/java/com/homework/App.java
+++ src/generalcavazoscommanderapp/src/main/java/com/homework/App.java
@@ -1,10 +1,28 @@
 package com.homework;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class App {
 
    private static JSONArray commandJSONArray;
+
+   public static void printMenu(){
+    System.out.println("----------------------------------------------------------");
+    System.out.println("General Cavazos COmmander App");
+    System.out.println("----------------------------------------------------------");
+    printMenuCommand('i', "Issue a command");
+    printMenuCommand('l', "List all of the commands");
+    printMenuCommand('u', "Undeo the last Command");
+    printMenuCommand('r', "Redo the last command");
+    printMenuCommand('q',"QUIT");
+    System.out.println("----------------------------------------------------------");
+   }
+
+   public static void printMenuCommand(Character command, String desc) {
+        System.out.printf("%s\t%s\n", command, desc);
+    }
+
   public static void main(String[] args){
     
     String fileName = "/Users/jerom/Documents/GitHub/class-java/general-cavazos/undoredo/src/main/java/com/cavazos/commands.json";
@@ -16,7 +34,11 @@ public class App {
 
     Character command = '_';
 
-    while ( commandd)
+    while (command != 'q'){
+        
+    }
 
   }
+
+
 }
