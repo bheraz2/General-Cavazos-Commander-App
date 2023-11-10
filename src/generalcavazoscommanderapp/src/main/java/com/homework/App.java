423394a374075faffb4d1edf0e443b8246ab4be4
Bryan Heraz <bherazbackup2@gmail.com>
2023-11-09 17:10:20 -0800

    redoing the code again, orginizing

--- src/generalcavazoscommanderapp/src/main/java/com/homework/App.java
+++ src/generalcavazoscommanderapp/src/main/java/com/homework/App.java
@@ -2,60 +2,12 @@ package com.homework;
 
 import java.util.Scanner;
 
-public class App 
-{
+public class App {
+
+   private static JSONArray commandJSONArray;
   public static void main(String[] args){
-
-      Scanner scan = new Scanner(System.in);
-    Character command = '_';
     
-    
+    String fileName = "/Users/jerom/Documents/GitHub/class-java/general-cavazos/undoredo/src/main/java/com/cavazos/commands.json";
 
-    // loop until user quits
-    while (command != 'q') {
-      printMenu();
-      System.out.print("Enter a command: ");
-      command = menuGetCommand(scan);
-
-      executeCommand(scan, command);
-    }
-
-    scan.close();      
-        
-    }
-
-    private static void printMenuline() {
-        System.out.println(
-      "----------------------------------------------------------"
-    );
-    }
-
-    private static void printMenuCommand(Character command, String desc ) {
-        System.out.printf("%s\t%s\n", command, desc);
-      }
-    
-
-    //Print the menu 
-    public static void printMenu(){
-        printMenuline();
-        System.out.println("General Cavazos COmmander App");
-        printMenuline();
-        printMenuCommand('i', 'Issue a command');
-        printMenuCommand('l', 'List all of th ecommans');
-        printMenuCommand('u', "Undo the last command that was issued");
-        printMenuCommand('r', 'Redo the last command that was issued');
-        printMenuCommand('q', 'Quit');
-        printMenuline();
-
-    }
-
-    public static char menuGetCommand(Scanner scanner){
-        String input = scanner .nextLine().toLowerCase();
-        if (input.length() > 0){
-            return input.charAt(0);
-        }  
-        return '_';
-    } 
-
-    public static void 
+  }
 }
