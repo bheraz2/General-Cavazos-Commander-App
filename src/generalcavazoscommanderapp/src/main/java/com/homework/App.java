fa80d08a38c2a4997659079b5abc759c8fe2d004
Bryan Heraz <bherazbackup2@gmail.com>
2023-11-09 17:55:01 -0800

    just declared the class will now work on the print menu for the program

--- src/generalcavazoscommanderapp/src/main/java/com/homework/App.java
+++ src/generalcavazoscommanderapp/src/main/java/com/homework/App.java
@@ -8,6 +8,15 @@ public class App {
   public static void main(String[] args){
     
     String fileName = "/Users/jerom/Documents/GitHub/class-java/general-cavazos/undoredo/src/main/java/com/cavazos/commands.json";
+    commandJSONArray = JSONFile.readArray(fileName);
+    
+    Stack<String> commandStack = new Stack<String>();
+    Stack<String> redoStack = new Stack<String>();
+    Scanner scanner = new Scanner(System.in);
+
+    Character command = '_';
+
+    while ( commandd)
 
   }
 }
